4339c2ffb621aaa6b92fa9d7bb9bf930409ec40a
ElSantanax <josesantanaserrano130598@gmail.com>
2025-09-02 12:14:09 -0500

    Atributos en Cypress

--- cypress/e2e/integration/primer_test.cy.js
+++ cypress/e2e/integration/primer_test.cy.js
@@ -2,8 +2,16 @@
 
 describe('Carga la pagina principal', () => {
   it('Carga la pagina principal', () => {
+
     cy.visit('http://127.0.0.1:5500/index.html');
-    cy.contains('h1', 'Administrador de Pacientes de Veterinaria');
-    cy.get('h1').should('exist');
+
+    // Ver si el TITULO se muestra
+    cy.contains('[data-cy="titulo-proyecto"]', 'Administrador de Pacientes de Veterinaria');
+    cy.get('[data-cy="titulo-proyecto"]').should('exist');
+
+    // Ver si las CITAS se muestran
+    cy.get('[data-cy="heading-titulo"]')
+      .invoke('text')
+      .should('equals', 'No hay Citas, comienza creando una')
   })
 })
